0eaed2fb35e9489d0fb134dff2132e3f1c91190e
Сергей Закиров <dh1kk@mail.ru>
2022-09-10 23:33:17 +0300

    ...

--- homework.py
+++ homework.py
@@ -1,5 +1,14 @@
+from typing import Dict, Type
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
+    # Почему не видит General_Message
+    general_message = ("Тип тренировки: {training_type}; "
+                       + "Длительность: {duration_HRS:.3f} ч.; "
+                       + "Дистанция: {distance:.3f} км; "
+                       + "Ср. скорость: {speed:.3f} км/ч; "
+                       + "Потрачено ккал: {calories:.3f}.")
 
     def __init__(self,
                  training_type: str,
@@ -9,24 +18,26 @@ class InfoMessage:
                  calories: float,
                  ) -> None:
         self.training_type = training_type
-        self.duration = round(duration, 3)
+        self.duration_HRS = round(duration, 3)
         self.distance = round(distance, 3)
         self.speed = round(speed, 3)
         self.calories = round(calories, 3)
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return self.general_message.format(training_type=self.training_type,
+                                           duration_HRS=self.duration_HRS,
+                                           distance=self.distance,
+                                           speed=self.speed,
+                                           calories=self.calories)
 
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
-    training_type = ''
+    LEN_STEP: float = 0.65
+    M_IN_KM: int = 1000
+    H_IN_MIN: int = 60
+    SQUARED: int = 2
+    training_type: str = ''
 
     def __init__(self,
                  action: int,
@@ -34,7 +45,7 @@ class Training:
                  weight: float,
                  ) -> None:
         self.action = action
-        self.duration = duration
+        self.duration_HRS = duration
         self.weight = weight
         pass
 
@@ -44,34 +55,36 @@ class Training:
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.get_distance() / self.duration
+        return self.get_distance() / self.duration_HRS
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise NotImplementedError
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.training_type, self.duration,
+        return InfoMessage(self.training_type, self.duration_HRS,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
 
 
 class Running(Training):
     """Тренировка: бег."""
-    training_type = 'Running'
+    training_type: str = 'Running'
+    coeff_calorie_1: int = 18
+    coeff_calorie_2: int = 20
 
     def get_spent_calories(self) -> float:
-        coeff_calorie_1 = 18
-        coeff_calorie_2 = 20
-        return ((coeff_calorie_1 * self.get_mean_speed()
-                - coeff_calorie_2) * self.weight
-                / self.M_IN_KM * self.duration * 60)
+        return ((self.coeff_calorie_1 * self.get_mean_speed()
+                - self.coeff_calorie_2) * self.weight
+                / self.M_IN_KM * self.duration_HRS * self.H_IN_MIN)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    training_type = 'SportsWalking'
+    training_type: str = 'SportsWalking'
+    coeff_calorie_1: float = 0.035
+    coeff_calorie_2: float = 0.029
 
     def __init__(self,
                  action: int,
@@ -83,18 +96,18 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        coeff_calorie_1 = 0.035
-        coeff_calorie_2 = 0.029
-        return ((coeff_calorie_1 * self.weight
-                + (self.get_mean_speed() ** 2 // self.height)
-                * coeff_calorie_2 * self.weight)
-                * self.duration * 60)
+        return ((self.coeff_calorie_1 * self.weight
+                + (self.get_mean_speed() ** self.SQUARED // self.height)
+                * self.coeff_calorie_2 * self.weight)
+                * self.duration_HRS * self.H_IN_MIN)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP = 1.38
-    training_type = 'Swimming'
+    LEN_STEP: float = 1.38
+    training_type: str = 'Swimming'
+    coeff_calorie_1: float = 1.1
+    coeff_calorie_2: int = 2
 
     def __init__(self,
                  action: int,
@@ -109,13 +122,11 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         return ((self.length_pool * self.count_pool)
-                / self.M_IN_KM / self.duration)
+                / self.M_IN_KM / self.duration_HRS)
 
     def get_spent_calories(self) -> float:
-        coeff_calorie_1 = 1.1
-        coeff_calorie_2 = 2
-        return ((self.get_mean_speed() + coeff_calorie_1)
-                * coeff_calorie_2 * self.weight)
+        return ((self.get_mean_speed() + self.coeff_calorie_1)
+                * self.coeff_calorie_2 * self.weight)
 
     def get_distance(self) -> float:
         return self.action * self.LEN_STEP / self.M_IN_KM
@@ -123,12 +134,15 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    class_dict = {
+    class_dict: Dict[str, Type[Training]] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
     }
-    return class_dict[workout_type](*data)
+    if workout_type not in class_dict.keys():
+        raise Exception('You have KeyError, check packages!')
+    else:
+        return class_dict[workout_type](*data)
 
 
 def main(training: Training) -> None:
